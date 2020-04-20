300b1bb5e9717700134bd91e38aff62b581ebd28
Shani McDonald <shanilmcdonald@gmail.com>
2020-04-20 12:20:49 +0100

    database connected

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
 
 const app = express()
+dotenv.config()
+
 
 // after express is invoked, declare the port that the server should choose from and provide a message
-app.listen(3000)
+app.listen(3000, () => console.log('server is up and running on port 3000'))
 app.get('/', (req, res) => {
   res.send('server is running on port 3000')
 })
+
+//After setting up database cluster on MongoDB Atlas, 
+mongoose.connect(
+  process.env.DB_CONNECTION, 
+  { useNewUrlParser: true, useUnifiedTopology: true  },
+  () => console.log('database is connected')
+)
